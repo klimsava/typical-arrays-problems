f13151293a1226def7037f9a1a49818a876d1241
Klim Savanovich <klimsava96@mail.ru>
2020-09-26 00:02:54 +0300

    made typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    return array !== undefined && array.length > 0 ? array.reduce((min, number) => {
+        if (min > number) {
+            min = number;
+        }
+
+        return min;
+    }, 0) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return array !== undefined && array.length > 0 ? array.reduce((max, number) => {
+        if (max < number) {
+            max = number;
+        }
+
+        return max;
+    }, 0) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return array !== undefined && array.length > 0 ? array.reduce((summ, number) => {
+        return summ + number;
+    }, 0) / array.length : 0;
 }
